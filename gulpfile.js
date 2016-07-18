7fd10cb89ab5e2ab6090c8a4eeedce65a041995b
Alexey Yanovski <alexey.yanovski1@gmail.com>
2016-07-18 21:01:17 +0100

    gulp setup for production

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var autoprefixer = require("gulp-autoprefixer");
 var imagemin = require("gulp-imagemin");
 var htmlmin = require("gulp-htmlmin");
 var cssmin = require("gulp-cssnano");
-var jscat = require("gulp-useref");
+var useref = require("gulp-useref");
 var uglify = require("gulp-uglify");
 var gulpIf = require("gulp-if");
 var autoprefixerOptions = {
@@ -21,16 +21,19 @@ gulp.task("serve", function(){
   });
   // watch sass files & run compile
   gulp.watch("./src/css/main.scss", ["sass"]);
-  // watch html, css, and js files
+  // reload browser on any css, js, or html change
   gulp.watch(["./src/js/*.js", "./src/css/*.css", "./src*.html"], bs.reload);
 });
+
+
+
 
 // Compile sass
 gulp.task("sass", function(){
   return gulp
     .src("./src/css/main.scss")
     .pipe(sass({
-      includePaths: ['./bower_components/foundation-sites/scss'],
+      includePaths: ['./src/bower_components/foundation-sites/scss'],
       style: "compressed",
       errLogToConsole: false,
       onError: function(err){
@@ -42,30 +45,47 @@ gulp.task("sass", function(){
     .pipe(bs.stream());
 });
 
+// minify js
 gulp.task("jsmin", function(){
   return gulp.src("./src/index.html")
-  .pipe(jscat())
+  .pipe(useref())
   .pipe(gulpIf("*.js", uglify()))
   .pipe(gulp.dest("dist"))
 });
 
-gulp.task("htmlmin", function(){
-  return gulp.src("./src*.html")
-  .pipe(htmlmin({collapseWhitespace:true}))
-  .pipe(gulp.dest("dist"));
-})
+// minify html
+// gulp.task("htmlmin", function(){
+//   return gulp.src("./src*.html")
+//   .pipe(htmlmin({collapseWhitespace:true}))
+//   .pipe(gulp.dest("dist"));
+// })
 
-
+// minify images
 gulp.task("imagemin", function(){
   gulp.src("./src/assets/images/*.jpg")
   .pipe(imagemin())
   .pipe(gulp.dest("./dist/assets/images"))
 });
 
+// minify css
 gulp.task("cssmin", function(){
-  return gulp.src("./src/css/main.css")
-  .pipe(cssmin())
-  .pipe(gulp.dest("./dist/css"));
+  return gulp.src("./src/index.html")
+  .pipe(useref())
+  .pipe(gulpIf("*.css", cssmin()))
+  .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("default", ["serve"]);
+gulp.task("build", ["imagemin", "cssmin", "jsmin"])
+
+// run public files without browser reload
+gulp.task("production", function(){
+  bs.init({
+    notify: false,
+    server: {
+      baseDir: "./dist"
+    }
+  });
+});
+
+// run dev files from /src with browser reload
+// gulp.task("serve", ["serve"]);
